ffacd91cc35c6ab7dc6cef5a9ec3b6eba968344d
Hanen Alkhafaji <hanenbalkhafaji@gmail.com>
2026-05-28 21:16:08 -0400

    fixed azure funtions

---
+++ api/index.js
@@ -0,0 +1,8 @@
+require('./src/functions/createRoom');
+require('./src/functions/joinRoom');
+require('./src/functions/getRoom');
+require('./src/functions/startGame');
+require('./src/functions/startVote');
+require('./src/functions/submitClue');
+require('./src/functions/submitVote');
+require('./src/functions/nextRound');
